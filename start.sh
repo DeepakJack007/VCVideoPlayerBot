e40df1733c8e34aaaa1a3594e602d3640aefd852
shukurenaibotcreate <93731368+shukurenaibotcreate@users.noreply.github.com>
2021-11-08 15:33:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning master branch...."
-  git clone https://github.com/ZauteKm/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/shukurenaibotcreate/VCVideoPlayerBot /VCVideoplayer
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/ZauteKm/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/shukurenaibotcreate/VCVideoPlayerBot -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
